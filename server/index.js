f7aa619949e66478ed0922eaf02a27f21d433385
Kate Leslie <thisizk8@gmail.com>
2017-03-16 13:43:45 -0500

    Setup Application

--- server/index.js
+++ server/index.js
@@ -6,15 +6,15 @@ module.exports = function (app) {
   const morgan  = require('morgan');
   app.use(morgan('dev'));
 
-  // Uncomment this for client-side routing
-  // app.use((req, res, next) => {
-  //   const acceptHeaders = req.headers.accept || [];
-  //   const hasHTMLHeader = acceptHeaders.indexOf('text/html') !== -1;
-  //
-  //   if (hasHTMLHeader) {
-  //     req.serveUrl = '/index.html';
-  //   }
-  //
-  //   next();
-  // });
+  //Uncomment this for client-side routing
+  app.use((req, res, next) => {
+    const acceptHeaders = req.headers.accept || [];
+    const hasHTMLHeader = acceptHeaders.indexOf('text/html') !== -1;
+
+    if (hasHTMLHeader) {
+      req.serveUrl = '/index.html';
+    }
+
+    next();
+  });
 };
